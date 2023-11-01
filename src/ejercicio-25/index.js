b10c5d51788cb49962de7559a7d43b4a81b8e642
epolov <epolov@gmail.com>
2023-11-01 18:29:12 +0100

    Ejercicio 25 - Numeros pares filter()

--- src/ejercicio-25/index.js
+++ src/ejercicio-25/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 25 - Filtrar por números pares
+
+const numeros = [1,2,3,4,5,66,73]
+const pares = numeros.filter((num) => num % 2 === 0)
+
+console.log(pares)
