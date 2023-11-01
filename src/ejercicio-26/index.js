16934e41d36562174f81e075d77e38e4407bc0ad
epolov <epolov@gmail.com>
2023-11-01 18:32:52 +0100

    Ejercicio 26 - concatenar arrays - concat()

--- src/ejercicio-26/index.js
+++ src/ejercicio-26/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 26 - Combinar arrays con concat() - concatenar
+
+const array1 = [20,15,10]
+const array2 = [3,33,629]
+
+const unir = array1.concat(array2)
+console.log(unir)
