c9c9bb07ecf77cfa1e4053b46b36d1c7969c55ce
epolov <epolov@gmail.com>
2023-11-01 10:35:06 +0100

    Ejercicio05 - Extraer porcion slice()

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// Ejercicio 05 - Extraer una porción de la cadena
+
+const texto = 'Extraer una porción de esta cadena'
+const extraer = texto.slice(0 ,5)
+
+console.log(extraer)
