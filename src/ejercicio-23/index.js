e15975260db959a6158b9faaa2b06a10b824a45c
epolov <epolov@gmail.com>
2023-11-01 12:13:59 +0100

    Ejercicio23 -  copiar valores subarray

--- src/ejercicio-23/index.js
+++ src/ejercicio-23/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+// EJERCICIO 23 - Copia superficial de Subarray
+
+const numeros = [1,5,8,9]
+const copianumeros = numeros.slice(0,2)
+
+console.log('Los valores del Array original son: ', numeros)
+console.log('Los valores copiados en el Subarray son: ', copianumeros)
