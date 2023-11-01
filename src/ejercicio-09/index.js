2800355b91adea2cd0da23cee59dabb480e7d3ad
epolov <epolov@gmail.com>
2023-11-01 10:58:12 +0100

    Ejercicio 09 - Eliminar espacios Trim()

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 09 - Eliminar espacios de la cadena
+
+const cadena = ' contiene espacios al pricipio y al final. '
+const cadenaTrim = cadena.trim(' ')
+
+console.log(cadenaTrim) // comprobacion quitando los espacios al principio  y final
+console.log(cadena) // comprobacion de cadena original
