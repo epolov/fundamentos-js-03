de038838462af8ea4b56c9420a1580f7f351f3cd
epolov <epolov@gmail.com>
2023-11-01 18:19:59 +0100

    Ejercicio 24 - separar cadena con split()

--- src/ejercicio-24/index.js
+++ src/ejercicio-24/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 24 - Dividir subcadenas con split()
+
+const cadena = 'Esto es una cadena separada por espacios'
+const separada = cadena.split(' ')
+
+console.log(separada)
