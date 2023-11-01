fee38b4964207fcc93b29ae5504b6e0cfa6e011e
epolov <epolov@gmail.com>
2023-11-01 10:50:26 +0100

    Ejercicio 07 - Reemplazar cadena método replace()

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 07 - Reemplazar subcadena replace()
+
+const cadena = 'esta es la cadena original'
+const cadenareplazada = cadena.replace('original', 'modificada')
+
+console.log(cadena) // comprobación cadena original
+console.log(cadenareplazada) // comprobacion cadena modificada
