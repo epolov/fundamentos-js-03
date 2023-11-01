ee0d0a9fb09e28ab95811e31fa221b5b3b87134a
epolov <epolov@gmail.com>
2023-11-01 11:05:51 +0100

    Ejercicio 11 - formatear decimales ToFixed

--- src/ejercicio-11/index.js
+++ src/ejercicio-11/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 11 - Formatear números decimales método "toFixed"
+
+const numero = 12.6523 // Los decimales se definen por punto no por comas
+const numeroToFixed = numero.toFixed(2)
+
+console.log(numeroToFixed)
