620eb63c4f28395eaa1545010466e06d71844c88
epolov <epolov@gmail.com>
2023-11-01 11:38:41 +0100

    Ejercicio 17 - redondeo al alza

--- src/ejercicio-17/index.js
+++ src/ejercicio-17/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+// EJERCICIO 17 - Redondear al alza
+
+const num1 = 5.23
+const num2 = 0.01
+
+const redondeoalta1 = Math.ceil(num1)
+const redondeoalta2 = Math.ceil(num2)
+
+console.log('El redonde al alza es:', redondeoalta1)
+console.log('El redonde al alza es:', redondeoalta2)
