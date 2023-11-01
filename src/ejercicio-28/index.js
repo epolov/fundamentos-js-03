e197ad022cf4f008fec74d4a9943d9561214c4ba
epolov <epolov@gmail.com>
2023-11-01 18:45:55 +0100

    Ejercicio 28 - Funcion map()

--- src/ejercicio-28/index.js
+++ src/ejercicio-28/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+// EJERCICIO 28 - Crear nuevo array con método map()
+
+const numeros = [10,13,23,66]
+//pasar el array a función map
+const map = numeros.map((x) => x * x);
+console.log(map)
