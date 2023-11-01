86bf3249278b5b5c6254eb8e1c7468b8e819ea0f
epolov <epolov@gmail.com>
2023-11-01 11:19:43 +0100

    Ejercicio 14 - Verificar numero entero

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 14 - Verificar número entero
+
+const valor1 = 25
+const valor2 = 6.1234
+
+
+console.log(Number.isInteger(valor1)) //la palabra Number debe ir en mayúscula
+console.log(Number.isInteger(valor2)) //la palabra Number debe ir en mayúscula
