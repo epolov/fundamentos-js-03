b99713ca45554d1eed8507cd584b4b2c32f2d143
epolov <epolov@gmail.com>
2023-11-01 11:22:40 +0100

    Ejercicio 15 - redondear a num entero más proximo

--- src/ejercicio-15/index.js
+++ src/ejercicio-15/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 15 - Redondear número
+
+const num = 14.624
+const redondear = Math.round(num) // Math, debe ir la M en mayúscula
+
+console.log(redondear)
