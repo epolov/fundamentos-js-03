aa8b4441ee6d6131bf4ad5394508fcffed023b39
epolov <epolov@gmail.com>
2023-11-01 10:54:55 +0100

    Ejercicio 08 - Metodo include()

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 08 - Verificar inclusión - método includes() -- permite comprobar si el valor se encuentra en la cadena (está incluido)
+
+const cadena = 'verificar inclusion'
+const cadenaInclusion = cadena.includes('verificar')
+
+console.log(cadenaInclusion) // devolverá true or false
