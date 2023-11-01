ba4e84db86baf23b1bbbbf715d1d031c4db8dd5e
epolov <epolov@gmail.com>
2023-11-01 11:30:11 +0100

    Ejercicio 16 - Redondear a la baja

--- src/ejercicio-16/index.js
+++ src/ejercicio-16/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+// EJERCICIO 16 - Redondear a la baja
+
+const num1 = 6.23
+const num2 = 2.73
+
+const redondeobajo = Math.floor(num1)
+const redondeobajo1 = Math.floor(num2)
+
+console.log('El redondeo a la baja de num1 es: ', redondeobajo)
+console.log('El redondeo a la baja de num2 es: ', redondeobajo1)
