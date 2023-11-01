b5424d159c6f4de27f91e650ce7052117eb5e42c
epolov <epolov@gmail.com>
2023-11-01 11:11:22 +0100

    Ejercicio 12 - convertir string a entero

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+//EJERCICIO 12 - Convertir a número entero con método parseInt()
+
+const numeroString = '13'
+const integerNum = parseInt(numeroString)
+
+console.log(integerNum)
