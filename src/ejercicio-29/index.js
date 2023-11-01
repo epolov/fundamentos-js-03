cea8627ff7fbf280ac5573fb6994312a313f26cd
epolov <epolov@gmail.com>
2023-11-01 18:50:48 +0100

    Ejercicio 29 - filter array solo pares

--- src/ejercicio-29/index.js
+++ src/ejercicio-29/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+// EJERCICIO 29 - Crear nuevo array con filter()
+
+const array = [1,2,55,66]
+// definicion de nuevo array filtrando solo con números pares
+const pares = array.filter((num) => num % 2 ===0)
+
+console.log(pares)
