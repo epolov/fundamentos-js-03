caa3373d33435361fe78b82e4fa01cdb5b8a3164
epolov <epolov@gmail.com>
2023-11-01 10:30:51 +0100

    Ejercicio 04 - posicion de cadena IndexOf

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// Ejercicio 04 - Encontrar posición de subcadena método indexOf
+
+const cadena = 'Buscar posicion de subcadena'
+const posicion = cadena.indexOf('1')
+
+console.log(posicion)
