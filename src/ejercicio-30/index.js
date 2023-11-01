d0f2c115a96f163e5b41ef280f3ac593a3c8ece7
epolov <epolov@gmail.com>
2023-11-01 18:54:10 +0100

    Ejercicio 30 - suma de valores del array

--- src/ejercicio-30/index.js
+++ src/ejercicio-30/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+// EJERCICIO 30 - Reducir a un solo valor
+
+const numeros = [1,23,44,568,6]
+const sumanumeros = numeros.reduce((total, num) => total + num, 0)
+
+console.log(sumanumeros)
