c24cf46699cfe4c06cdb8c94d1db3081eb3b6689
epolov <epolov@gmail.com>
2023-11-01 12:09:44 +0100

    Ejercicio 22 - Eliminar primer valor del array

--- src/ejercicio-22/index.js
+++ src/ejercicio-22/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+// EJERCICIO 22 - Eliminar el primer elemento del array shift()
+
+const animales = ['gato', 'Perro', 'Zorra', 'Gallo']
+const eliminaranimal = animales.shift()
+
+console.log('El animal eliminado del Array es: ', eliminaranimal)
+console.log('Los animales del Array son: ', animales)
