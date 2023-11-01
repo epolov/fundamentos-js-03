5d89c7c7ed0d8c061fc0b464c3285aa40c912188
epolov <epolov@gmail.com>
2023-11-01 11:44:37 +0100

    EJERCICIO 18 - Revisar

--- src/ejercicio-18/index.js
+++ src/ejercicio-18/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+
+// EJERCICIO 18 - Número aleatorio - método random /// PREGUNTAR POR ESTE EJERCICIO
+
+const sorteo = Math.floor(Math.random() * 10) + 1
+
+console.log(sorteo)
