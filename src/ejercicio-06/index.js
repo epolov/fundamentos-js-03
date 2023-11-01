1f1dde18b19cedffdfb5d55794f6b8b45f832f98
epolov <epolov@gmail.com>
2023-11-01 10:47:19 +0100

    Ejercicio 06 - dividir cadena metodo split()

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+
+// Ejercicio 06 - dividir subcadenas método split()
+
+const texto = 'Dividiremos la subcadena con el metodo split'
+const splittexto = texto.split(' ')
+
+console.log(splittexto)
