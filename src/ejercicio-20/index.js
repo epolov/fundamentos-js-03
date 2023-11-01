53e3563330115cd5fa1a4fc43fa9a9026d1690b6
epolov <epolov@gmail.com>
2023-11-01 12:01:47 +0100

    Ejercicio 20 - Eliminar ultimo valor pop()

--- src/ejercicio-20/index.js
+++ src/ejercicio-20/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+
+// EJERCICO 20 - Eliminar ultimo valor del array
+
+const colores = ['rojo', 'verde', 'amarillo']
+const eleminiarcolores = colores.pop()
+
+console.log('El color eliminado es. ', eleminiarcolores)
+console.log('Los valores que quedan en el Array son: ', colores)
